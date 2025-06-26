a8b80efab17fc1ba23dbf7ffe008f106d71a735a
Briansbum <freestone.alex@gmail.com>
2025-06-26 18:13:38 +0100

    first flake, p4

---
+++ nix/flakes/configuration.nix
@@ -0,0 +1,308 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+with pkgs; let
+  patchDesktop = pkg: appName: from: to: lib.hiPrio (
+    pkgs.runCommand "$patched-desktop-entry-for-${appName}" {} ''
+      ${coreutils}/bin/mkdir -p $out/share/applications
+      ${gnused}/bin/sed 's#${from}#${to}#g' < ${pkg}/share/applications/${appName}.desktop > $out/share/applications/${appName}.desktop
+      '');
+  GPUOffloadApp = pkg: desktopName: patchDesktop pkg desktopName "^Exec=" "Exec=nvidia-offload ";
+
+#  sddmHyprTheme = stdenv.mkDerivation {
+#    name = "sddm-hyprtheme";
+#    version = "1.0";
+#    src = /home/alex/devel/dotfiles/sddm/hyprtheme;  # path to your dotfiles theme directory
+#  
+#    dontBuild = true;
+#    installPhase = ''
+#      mkdir -p $out/share/sddm/themes/hyprtheme
+#      cp -r $src/* $out/share/sddm/themes/hyprtheme/
+#    '';
+#  };
+in
+{
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.useOSProber = true;
+
+  security.sudo.enable = true;
+
+  users.users.alex = {
+    isNormalUser = true;
+    description = "Alex";
+    extraGroups = ["wheel" "plugdev"];
+    packages = with pkgs; [
+      git
+      ghostty
+      brave
+      bitwarden
+      mako
+      tofi
+      discord
+      hyprcursor
+      hyprlock
+      lshw
+      jq
+      starship
+      unzip
+      python3Full
+      zig
+      helvum
+      pavucontrol
+      obs-studio
+      hyprpaper
+      vulkan-tools
+      nvidia-vaapi-driver
+      egl-wayland
+      darktable
+      fd
+      fzf
+      imagemagick
+      lxmenu-data
+      shared-mime-info
+      playerctl
+      clipse
+      wl-clipboard
+      rustup
+      cargo
+      gcc
+      go
+      terraform
+      shellcheck
+      python3
+      bash
+      helm
+      jsoncpp
+      yq
+      tflint
+      telegram-desktop
+      yazi
+      tree-sitter
+      udiskie
+      vlc
+      libraw
+      ueberzugpp
+      playerctl
+      brightnessctl
+      hypridle
+      yt-dlp
+      ffmpeg
+      luarocks
+      ripgrep
+      easyeffects
+      stremio
+    ];
+  };
+
+  services.gvfs.enable = true;
+  services.udisks2.enable = true;
+  services.devmon.enable = true;
+
+  services.displayManager.sddm = {
+    enable = false;
+    theme = "hyprtheme";
+#    extraPackages = [sddmHyprTheme];
+  };
+  
+  fonts.packages = [
+    pkgs.nerd-fonts.go-mono
+  ];
+
+  programs.fish.enable = true;
+  programs.bash = {
+    interactiveShellInit = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    withRuby = true;
+    withPython3 = true;
+    withNodeJs = true;
+    vimAlias = true;
+    viAlias = true;
+  };
+
+  nixpkgs.config.allowUnfree = true; 
+
+  # Enable OpenGL
+  hardware.graphics = {
+    enable = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = true;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = true;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    open = true;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
+  networking.hostName = "mandelbrot"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "Europe/London";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  
+  programs.hyprland.enable = true;
+  programs.hyprland.withUWSM = true;
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.variables.EDITOR = "nvim";
+
+  programs.steam = {
+    enable = true;
+    protontricks.enable = true;
+  };
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # hardware.pulseaudio.enable = true;
+  # OR
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    jack.enable = true;
+  };
+
+  services.mediatomb.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.users.alice = {
+  #   isNormalUser = true;
+  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  #   packages = with pkgs; [
+  #     tree
+  #   ];
+  # };
+
+  # programs.firefox.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+    environment.systemPackages = with pkgs; [
+      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      wget
+      cron
+      #sddmHyprTheme
+    ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
+
--- nix/flakes/flake.nix
+++ nix/flakes/flake.nix
@@ -1,1 +1,21 @@
-/etc/nixos/flake.nix
+{
+    description = "Baby's first nixos flake";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+        home-manager.url = "github:nix-community/home-manager";
+    };
+
+    outputs = {self, nixpkgs, ...}@inputs: {
+        nixosConfigurations = {
+            nixos = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux";
+                specialArgs = { inherit inputs; };
+                modules = [
+                    ./configuration.nix
+                    ./hardware/mandelbrot.nix
+                ];
+            };
+        };
+    };
+}

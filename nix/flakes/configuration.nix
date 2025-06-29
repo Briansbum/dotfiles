27d7bdba46fa03fe878e4c328ed853d8ef834187
Briansbum <freestone.alex@gmail.com>
2025-06-29 18:01:32 +0100

    I still do not understand flakes apparently

--- nix/flakes/configuration.nix
+++ nix/flakes/configuration.nix
@@ -2,7 +2,7 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{ config, pkgs, inputs, lib, ... }:
 
 with pkgs; let
   patchDesktop = pkg: appName: from: to: lib.hiPrio (

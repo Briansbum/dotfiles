76a3f3ff715b0435efd9fffa3c112eff35d8806f
Briansbum <freestone.alex@gmail.com>
2025-12-22 11:48:06 +0000

    add missing input

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
         };
     };
 
-    outputs = {self, nixpkgs, ...}@inputs: {
+    outputs = {self, nixpkgs, home-manager, ...}@inputs: {
         nixosConfigurations = {
             mandelbrot = nixpkgs.lib.nixosSystem {
                 system = "x86_64-linux";

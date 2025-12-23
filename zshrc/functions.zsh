c61bb34b67ab217bcb4367590fdd534f29ecf90c
Briansbum <freestone.alex@gmail.com>
2025-12-23 12:22:25 +0000

    remove zshrc

--- zshrc/functions.zsh
+++
@@ -1,8 +0,0 @@
-function fv {
-    fd $1 -X nvim
-}
-
-function gw {
-    pushd $(~/devel/dotfiles/scripts/gc.sh)
-}
-
--- zshrc/plugins.zsh
+++
@@ -1,4 +0,0 @@
-plugins=(git aws colored-man-pages kubectl kubectx kube-ps1 ssh-agent themes)
-
-zstyle :omz:plugins:ssh-agent identities bitwarden
-zstyle :omz:plugins:ssh-agent lazy yes

07259ca410f63244744a413c5c23a4adbf05d5e0
Alex Freestone <alex.freestone@gmail.com>
2026-04-11 20:17:36 +0100

    add steam to julia

--- nix/julia/configuration.nix
+++ nix/julia/configuration.nix
@@ -78,6 +78,14 @@
     '';
   };
 
+  programs.steam = {
+    enable = true;
+    protontricks.enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;

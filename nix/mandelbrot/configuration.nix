27fd20fc1ed384472c7f694d4b2cdacea3603a0b
Briansbum <freestone.alex@gmail.com>
2026-05-09 10:10:37 +0100

    set max number of boot gens to 10

--- nix/mandelbrot/configuration.nix
+++ nix/mandelbrot/configuration.nix
@@ -22,6 +22,7 @@ in
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
   security.sudo.enable = true;
 
